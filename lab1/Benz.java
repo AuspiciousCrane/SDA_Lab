e4761875fbb01caf698fda99d792b03cb9b4f72f
Chatchai Paisalpanich <frinttropystudy222@gmail.com>
2023-01-18 12:05:25 +0700

    First commit

---
+++ lab1/Benz.java
@@ -0,0 +1,7 @@
+public class Benz extends Vehicle{
+    @Override
+    public int getSpeed(){
+
+        return 1000;
+    }
+}
---
+++ lab1/Main.java
@@ -0,0 +1,26 @@
+public class Main{
+
+    public static void main(String args[]){
+        System.out.println("Hello software archiecture Lab 1");
+    
+        String vehicleType = "Honda";
+        switch(vehicleType){
+            case "Honda": {
+                System.out.println("10");
+                break;
+            }
+            case "Benz": {
+                System.out.println("1000");
+                break;
+            }
+            case "Tractor": {
+                System.out.println("5555");
+                break;
+            }
+        }
+    
+        Vehicle car = new Honda();
+        System.out.println(car.getSpeed());
+    }
+
+}
