21780677b4be078476544090edb6b282eb00bf0d
Janthanat Thanintantrakun <73385307+juntthanat@users.noreply.github.com>
2023-01-18 20:46:49 +0700

    feat: task1 pull

---
+++ lab2/task1/pull/Baby.java
@@ -0,0 +1,47 @@
+package task1.pull;
+import java.util.ArrayList;
+
+public class Baby implements Subject {
+
+	private ArrayList observers;
+	private boolean crying=false;
+	private int level=0;
+	private String babyname;
+	
+	public Baby(String name){
+		this.babyname=name;
+		observers=new ArrayList();
+	}
+	
+	public void notifyObservers() {
+		for (int i=0; i< observers.size(); i++) {
+			Observer observer = (Observer) observers.get(i);
+			observer.update();
+		}
+	}
+
+	public void registerObserver(Observer o) {
+		observers.add(o);
+
+	}
+
+	public void removeObserver(Observer o) {
+		int i = observers.indexOf(o);
+		if (i >=0) {
+			observers.remove(i);
+		}
+	}
+	
+	public void setData(boolean crying, int level) {
+		this.crying=crying;
+		this.level=level;
+		notifyObservers();
+	}
+
+	public String getName(){
+		return this.babyname;
+	}
+	public boolean getCrying(){
+		return this.crying;
+	}
+}
---
+++ lab2/task1/pull/TestBabyMonitor.java
@@ -0,0 +1,8 @@
+package task1.pull;
+public class TestBabyMonitor {
+	public static void main(String[] args) {
+				Baby marla = new Baby("marla");
+				BabyMonitorSimple livingRoom = new BabyMonitorSimple("kitchen ", marla);
+				marla.setData(true, 1);
+			}
+	}
