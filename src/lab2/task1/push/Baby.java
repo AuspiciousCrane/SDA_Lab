bac9a423ef093eec7e380133464348a20428676f
Chatchai Paisalpanich <frinttropystudy222@gmail.com>
2023-01-19 19:47:50 +0700

    Adde Baby.java

---
+++ src/lab2/task1/push/Baby.java
@@ -0,0 +1,35 @@
+package lab2.task1.push;
+
+import java.util.Observable;
+
+public class Baby extends Observable{
+    private String name;
+    private Boolean crying;
+    private Integer cryingLevel;
+    
+    Baby(String inName){
+        this.name = inName;
+        this.crying = false;
+        this.cryingLevel = 0;
+    }
+
+    public void setCry(Boolean inCrying, Integer inCryingLevel){
+        this.crying = inCrying;
+        this.cryingLevel = inCryingLevel;
+        setChanged();
+        notifyObservers();
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public Boolean isCrying(){
+        return this.crying;
+    }
+
+    public Integer getCryingLevel(){
+        return this.cryingLevel;
+    }
+    
+}
