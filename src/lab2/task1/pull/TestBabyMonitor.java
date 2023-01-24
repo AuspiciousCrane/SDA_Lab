197a59e87177f92e56807a7ec3978932045d7800
Chatchai Paisalpanich <frinttropystudy222@gmail.com>
2023-01-24 20:54:33 +0700

    Fix dangled } and format whitespace

--- src/lab2/task1/pull/TestBabyMonitor.java
+++ src/lab2/task1/pull/TestBabyMonitor.java
@@ -1,13 +1,12 @@
 package lab2.task1.pull;
 
-import lab2.task1.pull.*;
 public class TestBabyMonitor {
-	public static void main(String[] args) {
-				Baby marla = new Baby("marla");
-				BabyMonitorSimple kitchen = new BabyMonitorSimple("kitchen ");
-				marla.addObserver(kitchen);
-				marla.setData(true, 1);
-				marla.setData(true, 2);
-			}
-	}
+public static void main(String[] args) {
+        Baby marla = new Baby("marla");
+        BabyMonitorSimple kitchen = new BabyMonitorSimple("kitchen ");
+        marla.addObserver(kitchen);
+        marla.setData(true, 1);
+        marla.setData(true, 2);
+    }
 }
+
