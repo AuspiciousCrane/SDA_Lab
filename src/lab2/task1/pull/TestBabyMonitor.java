669ba645ed6503e892c77e56f43f3d80e7df4eca
Chatchai Paisalpanich <frinttropystudy222@gmail.com>
2023-01-24 19:38:30 +0700

    Reformat whitespaces of pull/TestBabyMonitor

--- src/lab2/task1/pull/TestBabyMonitor.java
+++ src/lab2/task1/pull/TestBabyMonitor.java
@@ -1,8 +1,8 @@
 package lab2.task1.pull;
 public class TestBabyMonitor {
 	public static void main(String[] args) {
-				Baby marla = new Baby("marla");
-				BabyMonitorSimple livingRoom = new BabyMonitorSimple("kitchen ", marla);
-				marla.setData(true, 1);
-			}
+		Baby marla = new Baby("marla");
+		BabyMonitorSimple livingRoom = new BabyMonitorSimple("kitchen ", marla);
+		marla.setData(true, 1);
 	}
+}
