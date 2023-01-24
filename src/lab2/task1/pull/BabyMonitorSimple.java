03f6d134d245b179b68c3f1c0f0823dbeeb425f0
Janthanat Thanintantrakun <73385307+juntthanat@users.noreply.github.com>
2023-01-24 19:37:39 +0700

    update: pull

--- src/lab2/task1/pull/BabyMonitorSimple.java
+++ src/lab2/task1/pull/BabyMonitorSimple.java
@@ -1,22 +1,24 @@
 package lab2.task1.pull;
+
 public class BabyMonitorSimple implements Observer {
 
 	private Baby mdata;
 	private String name;
+	private String location;
 	private boolean crying;
-	
+
 	public BabyMonitorSimple(String location, Baby d) {
-		this.mdata=d;
-		this.name=location;
+		this.mdata = d;
+		this.location = location;
 		mdata.registerObserver(this);
 	}
-		
+
 	public void display() {
 		if (crying) {
-			System.out.println("Monitor:" + name + " baby is crying");
+			System.out.println("Monitor:" + mdata.getName() + " baby is crying at location: " + location);
 		}
 	}
-	
+
 	public void turnOff() {
 		mdata.removeObserver(this);
 	}
