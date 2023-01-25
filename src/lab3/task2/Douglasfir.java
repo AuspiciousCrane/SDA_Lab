f91e6a5a1f59e2c7e6db65f89d747a18d7889ed6
Chatchai Paisalpanich <frinttropystudy222@gmail.com>
2023-01-25 14:49:05 +0700

    Added Douglasfir.java

---
+++ src/lab3/task2/Douglasfir.java
@@ -0,0 +1,14 @@
+package lab3.task2;
+
+public class Douglasfir extends Tree{
+
+    Douglasfir(){
+        description = "douglasfir decorated with, ";
+    }
+
+    @Override
+    public double cost() {
+        return 15;
+    }
+    
+}
