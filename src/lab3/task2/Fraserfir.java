16c47ca623c7f16bcc608a5acb18a7c76e088f72
Chatchai Paisalpanich <frinttropystudy222@gmail.com>
2023-01-25 14:46:51 +0700

    Added Fraserfir

---
+++ src/lab3/task2/Fraserfir.java
@@ -0,0 +1,13 @@
+package lab3.task2;
+
+public class Fraserfir extends Tree {
+    
+    public Fraserfir(){
+        description = "Fraser fir decorated with ";
+    }
+
+    @Override
+    public double cost(){
+        return 12;
+    }
+}
