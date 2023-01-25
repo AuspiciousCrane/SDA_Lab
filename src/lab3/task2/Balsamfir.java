9de6b5e2bddc2bed14b19a22d98c1d7f188ef90e
Chatchai Paisalpanich <frinttropystudy222@gmail.com>
2023-01-25 14:51:15 +0700

    Add Balsamfir

---
+++ src/lab3/task2/Balsamfir.java
@@ -0,0 +1,13 @@
+package lab3.task2;
+
+public class Balsamfir extends Tree {
+    
+    Balsamfir(){
+        description = "Balsamfir decorated with";
+    }
+
+    @Override
+    public double cost(){
+        return 5;
+    }
+}
