b3e3aaec51adb8adb311149c114ce551a3f14fb9
VirtuousCrane <ray.daroonpat.2020@gmail.com>
2023-01-27 00:41:58 +0700

    Finished: Lab 3 Task 3 Part 1

---
+++ src/lab3/task3/NVSlotBonus.java
@@ -0,0 +1,12 @@
+public class NVSlotBonus extends Slot {
+
+	public NVSlotBonus() {
+		cost=300;
+		name="Bonus";
+		software = "Linux";
+		components.add("Small");
+		components.add("Ticketinticketout");
+		components.add("CRT");
+		components.add("X86");
+	}
+}
---
+++ src/lab3/task3/WASlotProgressive.java
@@ -0,0 +1,12 @@
+public class WASlotProgressive extends Slot {
+
+	public WASlotProgressive() {
+		cost=300;
+		name="Progressive";
+		software = "Android";
+		components.add("Large");
+		components.add("coins");
+		components.add("reels");
+		components.add("ARM");
+	}
+}
