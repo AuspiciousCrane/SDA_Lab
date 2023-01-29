1bc392b5b3f87c57389c7b4bf95b3cb25904506e
Chatchai Paisalpanich <frinttropystudy222@gmail.com>
2023-01-29 10:06:54 +0700

    Added BallSilver.java

---
+++ src/lab3/task2/BallSilver.java
@@ -0,0 +1,21 @@
+package lab3.task2;
+
+public class BallSilver extends TreeDecorator {
+
+    BallSilver(Tree inTree){
+        this.tree = inTree;
+    }
+
+    @Override
+    public String getDescription() {
+        // TODO Auto-generated method stub
+        return this.tree + "ballSilver, ";
+    }
+
+    @Override
+    public double cost() {
+        // TODO Auto-generated method stub
+        return this.tree.cost() + 3;
+    }
+    
+}
