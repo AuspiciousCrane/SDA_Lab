366b443cba6f92efa3c1223ee48ef2853c4a956e
VirtuousCrane <ray.daroonpat.2020@gmail.com>
2023-02-26 23:46:06 +0700

    Finished: lab06-task03

---
+++ src/lab6/task3/facade/Water.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facade;
+
+/**
+ *
+ * @author boonjv
+ */
+public class Water {
+    // private static final int BOIL_TIME = 20;
+    private static final int BOIL_TIME = 1; // Reduced Boil Time
+	int x = 0;
+	// simulate boiling water
+	public void boilWater() {
+		// System.out.print("Boiling water for 20 seconds ");
+		System.out.print("Boiling water for 1 seconds ");
+		try {
+            while (x < BOIL_TIME) {
+                System.out.print(".");
+                x++;
+                Thread.sleep(1000);
+            }
+		}
+		catch (InterruptedException ie) { }
+        finally {
+            System.out.println();
+        }
+	}
+}
